1674a8ab5dbd341f4acfd62febbdb1cd31d9ace2
Kelly <kellyandstone@gmail.com>
2018-12-25 22:55:14 +1300

    use lodash

--- hello-GraqhQL/server/schema/schema.js
+++ hello-GraqhQL/server/schema/schema.js
@@ -1,7 +1,15 @@
 const graphql = require("graphql");
+const _ = require("lodash");
 
 const { GraphQLObjectType, GraqhQLString, GraqhQLSchema } = require("graphql");
 
+const books = [
+  { name: "BOOK1", genre: "IT", id: "1" },
+  { name: "BOOK2", genre: "Drama", id: "2" },
+  { name: "BOOK3", genre: "Romance", id: "3" }
+];
+
+console.log(_.find(books, { id: "2" }));
 const BookType = GraphQLObjectType({
   name: "Book",
   field: () => ({
@@ -19,6 +27,8 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraqhQLString } },
       resolve(parent, args) {
         // 从哪里得到数据，比如数据库或其他来源
+        // Mongodb mysql postgresql
+        _.find(books, { id: args.id });
       }
     }
   }
