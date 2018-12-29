f82674b6ae3805722988564bfeb9db214ea2e7bb
Kelly <kellyandstone@gmail.com>
2018-12-29 23:50:10 +1300

    install apollo

--- hello-GraqhQL/client/src/App.js
+++ hello-GraqhQL/client/src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
+import BookList from "./components/BookList";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider } from "react-apollo";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  link: new HttpLink(),
+  cache: new InMemoryCache()
+});
 
 class App extends Component {
   render() {
     return (
-      <div id="main">
-        <h1>Reading List</h1>
-      </div>
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Reading List</h1>
+          <BookList />
+        </div>
+      </ApolloProvider>
     );
   }
 }
