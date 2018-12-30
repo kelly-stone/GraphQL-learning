0406791ac9098d187cb41ba753439e8cd9c482e2
Kelly <kellyandstone@gmail.com>
2018-12-31 00:36:13 +1300

    using console.log, checkout different posrt

--- hello-GraqhQL/client/src/components/BookList.js
+++ hello-GraqhQL/client/src/components/BookList.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+
 import { graphql } from "react-apollo";
 import gql from "graphql-tag";
 
@@ -10,9 +11,11 @@ const getBookQuery = gql`
     }
   }
 `;
+//fetch all the books infor will pass to the props below
 
 class BookList extends Component {
   render() {
+    console.log(this.props); //after refreshing, there are two different data{} in console.log from different port
     return (
       <div>
         <ul id="book-list">
