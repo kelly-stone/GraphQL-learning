63f3a8a0f6b30badc51309174976dbbfb6c64c2b
Kelly <kellyandstone@gmail.com>
2018-12-31 15:16:48 +1300

    display loading... and books name

--- hello-GraqhQL/client/src/components/BookList.js
+++ hello-GraqhQL/client/src/components/BookList.js
@@ -14,13 +14,29 @@ const getBookQuery = gql`
 //fetch all the books infor will pass to the props below
 
 class BookList extends Component {
+  displayBooks = () => {
+    const data = this.props.data;
+    //from the console.log below(this.props), there is loading:true and false
+    if (data.error) {
+      return <div>undefined</div>;
+    } else if (data.loading) {
+      return <div>loading ......</div>;
+    } else {
+      //from console.log(this.props below)
+      return (
+        <ul id="book-list">
+          {data.books.map(book => {
+            return <li key={book.id}>{book.name}</li>;
+          })}
+        </ul>
+      );
+    }
+  };
   render() {
     console.log(this.props); //after refreshing, there are two different data{} in console.log from different port
     return (
       <div>
-        <ul id="book-list">
-          <li>Book name</li>
-        </ul>
+        <ul>{this.displayBooks()}</ul>
       </div>
     );
   }
