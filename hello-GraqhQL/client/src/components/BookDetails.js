bd0801ab53e8a7b13f7de3f2aa10219ea0375290
Kelly <kellyandstone@gmail.com>
2019-01-16 14:40:48 +1300

    render displaybookdetails

--- hello-GraqhQL/client/src/components/BookDetails.js
+++ hello-GraqhQL/client/src/components/BookDetails.js
@@ -3,13 +3,29 @@ import { graphql } from "react-apollo";
 import { getBookQuery } from "../queries/queries";
 
 export class BookDetails extends Component {
+  displayBookDetails = () => {
+    const { book } = this.props.data; //console.log(this.props)
+    if (book) {
+      return (
+        <div>
+          <h2>{book.name}</h2>
+          <p>{book.genre}</p>
+          <p>{book.author.name}</p>
+          <p>All books by this author:</p>
+          <ul className="other-books">
+            {book.author.books.map(item => {
+              return <li key={item.id}>{item.name}</li>;
+            })}
+          </ul>
+        </div>
+      );
+    } else {
+      return <div>No Book Selected...</div>;
+    }
+  };
   render() {
-    console.log(this.props); //after refreshing, there are two different data{} in console.log from different port
-    return (
-      <div id="book-details">
-        <p>Output book details here</p>
-      </div>
-    );
+    //console.log(this.props); //after refreshing, there are two different data{} in console.log from different port
+    return <div id="book-details">{this.displayBookDetails()}</div>;
   }
 }
 
